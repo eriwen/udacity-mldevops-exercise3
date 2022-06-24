253550f52306ef98badf19b255183355bfd2d5ad
Eric Wendelin <eric@gradle.com>
2022-06-24 15:37:46 -0700

    Add /predict sample to heroku testing script

--- heroku.py
+++ heroku.py
@@ -1,6 +1,26 @@
+import json
 import requests
 
-response = requests.get('https://fathomless-wildwood-07611.herokuapp.com/')
+get_response = requests.get('https://fathomless-wildwood-07611.herokuapp.com/')
+print(get_response.status_code)
+print(get_response.json())
 
-print(response.status_code)
-print(response.json())
+sample = {
+    "age": 49,
+    "workclass": "Local-gov",
+    "fnlgt": 268234,
+    "education": "HS-grad",
+    "education-num": 9,
+    "marital-status": "Married-civ-spouse",
+    "occupation": "Protective-serv",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 0,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
+}
+prediction_response = requests.post('https://fathomless-wildwood-07611.herokuapp.com/predict', data=json.dumps(sample))
+print(prediction_response.status_code)
+print(prediction_response.json())
